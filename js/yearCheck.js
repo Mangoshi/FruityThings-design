c33a4a6d96d67f462ea57e55584355cea6d0a9f7
Seán Óg <soggymoose@gmail.com>
2021-04-24 16:52:21 +0100

    Removed unused JS folder & yearCheck.js

--- js/yearCheck.js
+++
@@ -1,8 +0,0 @@
-(function () {
-	let currentYear = (new Date).getFullYear();
-	let copyYear = document.querySelectorAll('.year');
-	
-	for (let i = 0; i < copyYear.length; i++) {
-		copyYear[i].textContent = currentYear;
-	}
-}());
